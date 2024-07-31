d183c7345bf00eefeca9a710ef153237c07afab7
Dharmadeep <dharmadeepmadisetty@gmail.com>
2024-07-31 18:58:46 +0530

    Deploy 10.1

--- client/RedThred/src/App.jsx
+++ client/RedThred/src/App.jsx
@@ -90,7 +90,7 @@ const App = () => {
                 <div className="dark:text-white flex items-center sm:items-stretch text-nowrap sm:flex-col gap-2">
                   <button
                     onClick={() => navigate("/explore")}
-                    className="ml-[600px] sm:ml-0 sm:flex justify-start gap-2 border rounded-sm p-2 "
+                    className="ml-[650px] sm:ml-0 sm:flex justify-start gap-2 border rounded-sm p-2 "
                   >
                     <ExploreIcon />
                     Explore
