30c874b02edcaa2e16b3f81bf60e46c6d1a235ea
Dharmadeep <dharmadeepmadisetty@gmail.com>
2024-08-02 16:03:08 +0530

    Docs 1

--- client/RedThred/src/api/index.js
+++ client/RedThred/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://red-thread-nine.vercel.app" });
 
 // https://red-thread-nine.vercel.app
 
